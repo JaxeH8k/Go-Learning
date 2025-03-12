f7ab12f5207c63deb9190dc7966ca05eb3b7435a
JaxeH8k <jh@dangeroustoysforkids.com>
2025-03-12 13:31:52 -0700

    added a basic script to do some time estimate with n+1 iterations

--- N1SecondRequestDelaySim/main.go
+++ N1SecondRequestDelaySim/main.go
@@ -8,6 +8,22 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var nPlus int64
+	var totalWait int64
+	for i := 0; i <= 1000; i++ {
+		nPlus = nPlus + 1
+		// fake request
+		// measure something akin to: time.Sleep(time.Duration(nPlus) * time.Second)
+		totalWait = totalWait + nPlus
+
+	}
+	fmt.Printf("If you added a second between each request,\n"+
+		"excluding the time the request took to respond, it would\n"+
+		"take %f2 Days to complete 1000 requests.\n",
+		float64(float64(totalWait)/60/60/24))
 }
